00dfcb7195b8c6b75124243313b73e7d167e7db5
thiagola92 <thiagola92@gmail.com>
2022-09-03 15:16:48 -0300

    Add terraform files and actions files

---
+++ python.tf
@@ -0,0 +1,23 @@
+resource "github_repository_file" "python_test" {
+  for_each            = toset(data.github_repositories.la_catalog.names)
+  repository          = each.key
+  branch              = "main"
+  file                = ".github/workflows/python-test.yml"
+  content             = file("./workflows/python-test.yml")
+  commit_message      = "Update python-test.yml"
+  commit_author       = "actions"
+  commit_email        = "actions@github.com"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "python_publish" {
+  for_each            = toset(data.github_repositories.la_catalog.names)
+  repository          = each.key
+  branch              = "main"
+  file                = ".github/workflows/python-publish.yml"
+  content             = file("./workflows/python-publish.yml")
+  commit_message      = "Update python-publish.yml"
+  commit_author       = "actions"
+  commit_email        = "actions@github.com"
+  overwrite_on_create = true
+}
