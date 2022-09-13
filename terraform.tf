498e1c433bfc5a1994681860061268930a7e7522
thiagola92 <thiagola92@gmail.com>
2022-09-13 10:50:40 -0300

    Add terraform-apply and rename some actions job's

--- terraform.tf
+++ terraform.tf
@@ -9,3 +9,15 @@ resource "github_repository_file" "terraform_fmt" {
   commit_email        = "actions@github.com"
   overwrite_on_create = true
 }
+
+resource "github_repository_file" "terraform_apply" {
+  for_each            = toset(data.github_repositories.terraform.names)
+  repository          = each.key
+  branch              = "main"
+  file                = ".github/workflows/terraform-apply.yml"
+  content             = file("./res/terraform-apply.yml")
+  commit_message      = "Update terraform-apply.yml"
+  commit_author       = "actions"
+  commit_email        = "actions@github.com"
+  overwrite_on_create = true
+}
