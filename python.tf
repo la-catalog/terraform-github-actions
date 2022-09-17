112dfc8cd5a351a30e978e461f5d1786ca7c0d3a
thiagola92 <thiagola92@gmail.com>
2022-09-17 05:45:52 -0300

    Add action python-update.yml

--- python.tf
+++ python.tf
@@ -10,6 +10,18 @@ resource "github_repository_file" "python_test" {
   overwrite_on_create = true
 }
 
+resource "github_repository_file" "python_update" {
+  for_each            = toset(data.github_repositories.python.names)
+  repository          = each.key
+  branch              = "main"
+  file                = ".github/workflows/python-update.yml"
+  content             = file("./res/python-update.yml")
+  commit_message      = "Update python-update.yml"
+  commit_author       = "actions"
+  commit_email        = "actions@github.com"
+  overwrite_on_create = true
+}
+
 resource "github_repository_file" "python_publish" {
   for_each            = toset(data.github_repositories.python_package.names)
   repository          = each.key
