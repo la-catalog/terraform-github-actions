1c85922a65a601c44e914e2fedfdc29b6099b716
thiagola92 <thiagola92@gmail.com>
2022-11-17 06:20:17 -0300

    Change the workflows name to use snake_case

--- python.tf
+++ python.tf
@@ -2,8 +2,8 @@ resource "github_repository_file" "python_test" {
   for_each            = toset(data.github_repositories.python.names)
   repository          = each.key
   branch              = "main"
-  file                = ".github/workflows/python-test.yml"
-  content             = file("./res/python-test.yml")
+  file                = ".github/workflows/python_test.yml"
+  content             = file("./res/python_test.yml")
   commit_message      = "Update python-test.yml"
   commit_author       = "github-actions"
   commit_email        = "github-actions[bot]@users.noreply.github.com"
@@ -22,8 +22,8 @@ resource "github_repository_file" "python_update" {
   for_each            = toset(data.github_repositories.python.names)
   repository          = each.key
   branch              = "main"
-  file                = ".github/workflows/python-update.yml"
-  content             = file("./res/python-update.yml")
+  file                = ".github/workflows/python_update.yml"
+  content             = file("./res/python_update.yml")
   commit_message      = "Update python-update.yml"
   commit_author       = "github-actions"
   commit_email        = "github-actions[bot]@users.noreply.github.com"
@@ -42,8 +42,8 @@ resource "github_repository_file" "python_publish" {
   for_each            = toset(data.github_repositories.python_package.names)
   repository          = each.key
   branch              = "main"
-  file                = ".github/workflows/python-publish.yml"
-  content             = file("./res/python-publish.yml")
+  file                = ".github/workflows/python_publish.yml"
+  content             = file("./res/python_publish.yml")
   commit_message      = "Update python-publish.yml"
   commit_author       = "github-actions"
   commit_email        = "github-actions[bot]@users.noreply.github.com"
--- terraform.tf
+++ terraform.tf
@@ -2,8 +2,8 @@ resource "github_repository_file" "terraform_fmt" {
   for_each            = toset(data.github_repositories.terraform.names)
   repository          = each.key
   branch              = "main"
-  file                = ".github/workflows/terraform-fmt.yml"
-  content             = file("./res/terraform-fmt.yml")
+  file                = ".github/workflows/terraform_fmt.yml"
+  content             = file("./res/terraform_fmt.yml")
   commit_message      = "Update terraform-fmt.yml"
   commit_author       = "actions"
   commit_email        = "actions@github.com"
@@ -22,8 +22,8 @@ resource "github_repository_file" "terraform_apply" {
   for_each            = toset(data.github_repositories.terraform.names)
   repository          = each.key
   branch              = "main"
-  file                = ".github/workflows/terraform-apply.yml"
-  content             = file("./res/terraform-apply.yml")
+  file                = ".github/workflows/terraform_apply.yml"
+  content             = file("./res/terraform_apply.yml")
   commit_message      = "Update terraform-apply.yml"
   commit_author       = "actions"
   commit_email        = "actions@github.com"
